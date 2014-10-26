eee4d3feaef5587c4973b1af57270a2bef0258ce
Jasper O'Leary <jhaazpr@gmail.com>
2014-10-25 21:03:10 -0700

    actually actually add files

---
+++ app.js
@@ -0,0 +1,24 @@
+// [module names]
+var app = angular.module('templateApp', ['ui.router', 'ngTouch']); 
+
+// app.config(["$stateProvider", "$urlRouterProvider", "$locationProvider",
+// 	function($stateProvider, $urlRouterProvider, $locationProvider) {
+
+//   $urlRouterProvider.otherwise("/"); // render home if no other hits for state
+
+//   $stateProvider
+//     .state('home', {
+//       url: '/',
+//       templateUrl: '/views/home.html'
+//     })
+//     .state('blog', {
+//       url: '/blog',
+//       title: 'Blog',
+//       templateUrl: '/views/blog.html'
+//     })
+//     .state('schadenfreude', {
+//       url: '/schadenfreude',
+//       title: 'Schadenfreude',
+//       templateUrl: '/views/schadenfreude.html'
+//     })
+// }]);
